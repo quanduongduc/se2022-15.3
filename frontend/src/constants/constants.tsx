654d78f6e80245130ce12ae8afe253b3351bb9da
Duong Duc Quan <quanduongduc@gmail.com>
2022-12-21 13:56:09 +0700

    rename compose env name

--- frontend/src/constants/constants.tsx
+++ frontend/src/constants/constants.tsx
@@ -1,2 +1,4 @@
 export const API_URL: string =
-    process.env.NODE_ENV == 'production' ? '/api' : 'http://localhost:5000/api';
+    process.env.NODE_ENV == 'production'
+        ? '/api/v1'
+        : 'http://localhost:5000/api/v1';
