ac3a0b08f4906f694ae31ec171e9acdf42f0067b
Duong Duc Quan <quanduongduc@gmail.com>
2022-12-25 21:38:21 +0700

    user and auth controller implement

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,64 @@
+import { userController } from './user.controller';
+import * as bcrypt from 'bcrypt';
+import { NextFunction, Request, Response } from 'express';
+import { HttpStatus } from '../utils';
+import { HttpException } from '../utils';
+import * as jwt from 'jsonwebtoken';
+import { config } from '../configs';
+
+export const login = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const { userName, password } = req.body;
+
+        const user: any = await userController.findOne({
+            userName: userName
+        });
+        if (!user) {
+            return next(
+                new HttpException(
+                    HttpStatus.NOT_IMPLEMENTED,
+                    'User or Password is incorrect'
+                )
+            );
+        }
+        const isMatchedPassword = await bcrypt.compare(
+            password,
+            user?.password
+        );
+        if (!isMatchedPassword) {
+            return next(
+                new HttpException(
+                    HttpStatus.BAD_REQUEST,
+                    'User or Password is incorrect'
+                )
+            );
+        }
+
+        const secondOfMonth = 2592000;
+
+        const token = jwt.sign(user.toJSON(), config.JWT_SECRET);
+        res.cookie('accessToken', token, {
+            maxAge: secondOfMonth,
+            httpOnly: true
+        });
+        res.json({
+            message: 'Login successfully',
+            accessToken: token
+        });
+    } catch (error) {
+        console.log(error);
+
+        next(
+            new HttpException(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                'Intenal Server Error'
+            )
+        );
+    }
+};
+
+export const register = userController.createUser;
---
+++ backend/src/controllers/index.ts
@@ -0,0 +1,4 @@
+import { login, register } from './auth.controller';
+import { userController } from './user.controller';
+
+export { userController, login, register };
