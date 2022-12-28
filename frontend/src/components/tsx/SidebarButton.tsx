e4d39f0d0aaf5c6e54668b8af3a5a2ad3b9a324a
FADEGOD\LENOVO <nghiagod121122@gmail.com>
2022-12-28 22:29:51 +0700

    css for component temporary

---
+++ frontend/src/components/tsx/SidebarButton.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from 'react';
+
+const SidebarButton = (): ReactElement => {
+    return (
+        <div>
+            <p>side bar</p>
+        </div>
+    );
+};
+
+export default SidebarButton;
