2133b7c643d62fe81b1076e6f7e1eaa659747ced
Duong Duc Quan <quanduongduc@gmail.com>
2023-01-09 11:23:07 +0700

    add test

--- backend/src/test/integration/route/auth.spec.ts
+++ backend/src/test/integration/route/auth.spec.ts
@@ -1,4 +1,4 @@
-import { HttpStatus } from '';
+import { HttpStatus } from '../../../utils';
 import { jsonHeaders, request } from '../../helpers';
 import { connectDB } from '../../../configs';
 import mongoose from 'mongoose';
